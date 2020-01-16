3b27c05838e27eed61d5b9f20eb8064b34d13596
kazuki <s1250040a@gmail.com>
2020-01-16 23:02:34 +0900

    fix

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ def main():
     messages = TextSendMessage(text="こんにちは")
     line_bot_api.push_message(line_user_id, messages=messages)
 
-schedule.every().day.at("23:04").do(main)
+schedule.every().day.at("23:06").do(main)
 
 while True:
   schedule.run_pending()
